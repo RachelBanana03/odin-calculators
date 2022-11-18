7684ffaabf706db82c48b365d6c2dc7ec2fe0e5b
Rachel Banana <117894827+RachelBanana03@users.noreply.github.com>
2022-11-19 04:36:34 +0800

    Add syntax error checker on math exp

--- js/calculator.js
+++ js/calculator.js
@@ -2,6 +2,7 @@
 const NUM_REG = /\d+(?:\.\d+)*/; // A number with optional demical places
 const MUL_DIV_REG = new RegExp(`${NUM_REG.source}[*\\/]${NUM_REG.source}(?:[*\\/]${NUM_REG.source})*`, "g"); // Chain of multiplication/divisions
 const ADD_SUB_REG = new RegExp(`${NUM_REG.source}[+-]${NUM_REG.source}(?:[+-]${NUM_REG.source})*`, "g"); // Chain of additions/subtractions
+const MATH_REG = new RegExp(`^${NUM_REG.source}(?:[*\\/+-]${NUM_REG.source})*$`); // match a valid math expression
 
 // /**
 //  * Evaluate a mathematical expression
@@ -9,8 +10,9 @@ const ADD_SUB_REG = new RegExp(`${NUM_REG.source}[+-]${NUM_REG.source}(?:[+-]${N
 //  * @returns {number} 
 //  */
 function calculate(mathExp) {
-    // check for syntax errors
-    // check for overflow
+    // trim and check for syntax errors
+    mathExp = mathExp.replace(/\s/g, "");
+    if (!mathExp.match(MATH_REG)) return "SYNTAX ERROR!";
 
     // Match and evaluate all multiplications/divisions first
     mathExp = mathExp.replace(MUL_DIV_REG, multiplyDivide);
@@ -28,5 +30,9 @@ function addSubtract(addSubExp) {
 }
 
 
-const a = calculate("9342+3243*5242/6545-5454/4545/34/45");
-console.log(a);
+// test
+const a = "*9342+3243*5242/6545-5454/4545/34/45";
+const b = "3/0*2";
+const c = "2+2";
+const d = "24342.3";
+console.log(calculate(d));
