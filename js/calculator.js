51ff8704cbe43fef3ae4a91574366e2af49b13b7
Rachel Banana <117894827+RachelBanana03@users.noreply.github.com>
2022-11-19 04:53:12 +0800

    Clean calculator.js file and export

--- js/calculator.js
+++ js/calculator.js
@@ -4,12 +4,10 @@ const MUL_DIV_REG = new RegExp(`${NUM_REG.source}[*\\/]${NUM_REG.source}(?:[*\\/
 const ADD_SUB_REG = new RegExp(`${NUM_REG.source}[+-]${NUM_REG.source}(?:[+-]${NUM_REG.source})*`, "g"); // Chain of additions/subtractions
 const MATH_REG = new RegExp(`^${NUM_REG.source}(?:[*\\/+-]${NUM_REG.source})*$`); // match a valid math expression
 
-// /**
-//  * Evaluate a mathematical expression
-//  * @param {string} mathExp 
-//  * @returns {number} 
-//  */
+// Calculation functions
 function calculate(mathExp) {
+    if (mathExp === "") return "EMPTY!";
+
     // trim and check for syntax errors
     mathExp = mathExp.replace(/\s/g, "");
     if (!mathExp.match(MATH_REG)) return "SYNTAX ERROR!";
@@ -22,17 +20,11 @@ function calculate(mathExp) {
 }
 
 function multiplyDivide(mulDivExp) {
-    return mulDivExp.split(/(?=[*\/])/g).reduce((n, opNum) => opNum[0]=="*"? +n*Number(opNum.slice(1)): +n/Number(opNum.slice(1)));
+    return mulDivExp.split(/(?=[*\/])/g).reduce((n, opNum) => opNum[0] == "*" ? +n * Number(opNum.slice(1)) : +n / Number(opNum.slice(1)));
 }
 
 function addSubtract(addSubExp) {
-    return addSubExp.split(/(?=[+-])/g).reduce((n, opNum) => opNum[0]=="+"? +n+Number(opNum.slice(1)): +n-Number(opNum.slice(1)));
+    return addSubExp.split(/(?=[+-])/g).reduce((n, opNum) => opNum[0] == "+" ? +n + Number(opNum.slice(1)) : +n - Number(opNum.slice(1)));
 }
 
-
-// test
-const a = "*9342+3243*5242/6545-5454/4545/34/45";
-const b = "3/0*2";
-const c = "2+2";
-const d = "24342.3";
-console.log(calculate(d));
+export {calculate};
---
+++ js/index.js
@@ -0,0 +1,3 @@
+import calculate from "./index.js";
+
+console.log(calculate("3+3 -3"));
