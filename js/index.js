b4371a59da37123dabc45d86a6f5f95a7fc89bcd
Rachel Banana <117894827+RachelBanana03@users.noreply.github.com>
2022-11-19 19:37:45 +0800

    Add eventlisteners to key nums/ops

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const displayElement = document.querySelector("#display");
 const inputElement = document.querySelector("#input-1");
+const keyNumOpElements = document.querySelectorAll(".key-num, .key-op");
 
 inputElement.addEventListener("keydown", e => {
     if (e.key === "Enter") {
@@ -8,3 +9,8 @@ inputElement.addEventListener("keydown", e => {
         displayElement.textContent = result;
     }
 });
+
+keyNumOpElements.forEach(keyNum => keyNum.addEventListener("click", e => {
+    console.log(e.target.getAttribute("data-key"));
+    inputElement.value += e.target.getAttribute("data-key");
+}));
